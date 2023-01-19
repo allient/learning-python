b72dc86947be72128a5c19f5fd04e2f61aed0bd3
Leonel Rivas <leonelrivas.rl@gmail.com>
2023-01-19 11:47:30 -0300

    oop finished

---
+++ basic/funcional_programming/itertools.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+items = ['x', 'y']
+
+#your code goes here
+
+print(list(permutations(items))) 
+
+
+"""
+itertools
+
+
+You are given a list of items, and need to find all the possible orders of the items.
+The output should be a list, containing all possible orders.
+
+Sample Input
+['a', 'b']
+
+Sample Output
+[('a', 'b'), ('b', 'a')]
+The itertools module contains many useful functions that can achieve the above mentioned task.
+"""
